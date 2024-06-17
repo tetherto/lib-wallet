55b4656520f18750df336f54ec74578efbaeb95d
rbndg <rbandegi@gmail.com>
2024-06-17 20:31:33 +1000

    wip: new modules folder for reusable util between different assets. hdwallet class

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const Currency = require('./src/lib/currency.js')
 const WalletPay = require('./src/lib/wallet-pay.js')
 const Wallet = require('./src/lib/wallet.js')
-
+const HdWallet = require('./src/modules/hdwallet.js')
 module.exports = {
   Currency,
   WalletPay,
-  Wallet 
-
+  Wallet,
+  HdWallet
 }
